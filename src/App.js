bb1e2ca4071210cdd6ade86ea4822486d8b597f9
davegb313 <davidbesnovatiy@gmail.com>
2019-08-07 11:52:46 +0300

    func totalVotes working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import USA from './USA';
+import votes from './votes';
+
+const colors = ['red', 'blue', 'green'];
+class App extends React.Component {
+
+  state = {
+    reds: 0,
+    blues: 0,
+    greens: 0,
+  };
+
+  changeStateColor = event => {
+    this.setState(
+      {
+        [event.target.id]: colors[
+          (colors.indexOf(this.state[event.target.id]) + 1) % colors.length
+        ],
+      },
+      this.calculateVotes,
+    );
+  };
+
+  calculateVotes = () => {
+    let totalReds = 0;
+    let totalBlues = 0;
+    let totalGreens = 0;
+
+    Object.keys(votes).forEach(key => {
+      if (this.state[key] === 'red') {
+        totalReds += votes[key];
+      } else if (this.state[key] === 'blue') {
+        totalBlues += votes[key];
+      } else if (this.state[key] === 'green') {
+        totalGreens += votes[key];
+      }
+    });
+
+    this.setState({
+      reds: totalReds,
+      blues: totalBlues,
+      greens: totalGreens,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <USA colors={this.state} onClick={this.changeStateColor} />
+        <div className="winner">
+          <div><span />{this.state.reds}</div>
+          <div><span />{this.state.blues}</div>
+          <div><span />{this.state.greens}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
